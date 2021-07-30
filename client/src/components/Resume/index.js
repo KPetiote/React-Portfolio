dfd3b28db59eafc63c01341c28906b46e37d4460
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-30 09:53:56 -0400

    Finished Resume Page

--- client/src/components/Resume/index.js
+++ client/src/components/Resume/index.js
@@ -2,14 +2,30 @@
 // ---------------------------------------------------------------------------
 
 import React from 'react';
-// import "./style.css";
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import file from './kenneth-petiote.pdf';
 
-function Resume() {
-    return (
-        <div>
+export default function Resume() {
+  return (
+    <main className="container resume-padding-top">
+      <div className="card border-0 shadow my-5">
+        <div className="card-body p-5">
+          <div className="row">
+            <div className="col-xs-5 col-md-6">
+              <Document file={file}>
+                <Page pageNumber={1} /> 
+              </Document>
+            </div>
 
+            <div className="col-xs-5 col-md-6">
+            <Document file={file}>
+              <Page pageNumber={2} /> 
+            </Document>
+            </div>
+
+            </div>
+          </div>
         </div>
-    )
+    </main>
+  );
 }
-
-export default Resume;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,63 +2,61 @@
 // ---------------------------------------------------------------------------
 
 import React, { Component } from "react";
+// import Resume from "../components/Resume";
 
 class Home extends Component {
 
     render() {
         return (
-            <div>
-                <main className="container padding-top">
-                        <div className="card border-0 shadow my-5">
-                            <div className="card-body p-5">
-                                <h1><i>A little bit about myself...</i></h1>
-                                <div className="row">
-                                    <div className="col-xs-6 col-md-6">
-                                        <p className="lead">
-                                            My name is Kenneth Petiote and I am a full-time IT Professional. 
-                                            <br></br> 
-                                            My knack for learning about technology has lead me to pursuit a certification in Full Stack Web Development. 
-                                            As an aspiring Developer, I enrolled and completed an extensive Web Development Program at Rutgers's Coding Bootcamp.
-                                            <br></br>
-                                        </p>
-                                        <p className="lead">
-                                            During the program I've acquired skills in HTML, CSS, JavaScript, MySQL, Node.js, React.js, and Third Party API's.
-                                            Definitely eager and looking forward to branching into software developing as a new career path.
-                                            I have the ability to adapt and be versatile in any environment, whether in a team or working independently.                                
-                                        </p>
-                                        <p className="lead">
-                                            Aside from coding, I love photography, gaming, and working out.
-                                            <br></br>
-                                            I also own a side business as an event photographer.
-                                            
-                                        </p>
+            <main className="container home-padding-top">
+                <div className="card border-0 shadow my-5">
+                    <div className="card-body p-5">
+                        <h1><i>A little bit about myself...</i></h1>
+                        <div className="row">
+                            <div className="col-xs-6 col-md-6">
+                                <p className="lead">
+                                    My name is Kenneth Petiote and I am a full-time IT Professional. 
+                                    <br></br> 
+                                    My knack for learning about technology has lead me to pursuit a certification in Full Stack Web Development. 
+                                    As an aspiring Developer, I enrolled and completed an extensive Web Development Program at Rutgers's Coding Bootcamp.
+                                    <br></br>
+                                </p>
 
-                                        <h4><b>Let's connect via the links below:</b></h4>
+                                <p className="lead">
+                                    During the program I've acquired skills in HTML, CSS, JavaScript, MySQL, Node.js, React.js, and Third Party API's.
+                                    Definitely eager and looking forward to branching into software developing as a new career path.
+                                    I have the ability to adapt and be versatile in any environment, whether in a team or working independently.                                
+                                </p>
 
-                                        <a href="tel:856-236-6874">
-                                            <i class="fas fa-envelope-square fa-3x color-green padding-right"></i>
-                                        </a>   
+                                <p className="lead">
+                                    Aside from coding, I love photography, gaming, and working out.
+                                    <br></br>
+                                    I also own a side business as an event photographer.
+                                </p>
 
-                                        <a href="https://github.com/KPetiote">
-                                            <i className="fab fa-github-square fa-3x color-green padding-right"></i>
-                                        </a>  
+                                <h4><b>Let's connect via the links below:</b></h4>
 
-                                        <a href="https://www.linkedin.com/in/kenneth-petiote/">
-                                            <i className="fab fa-linkedin fa-3x color-green"></i>
-                                        </a>
+                                <a href="tel:856-236-6874">
+                                    <i class="fas fa-envelope-square fa-3x color-green padding-right"></i>
+                                </a>   
 
-                                    </div>
-                                    <div className="col-xs-7 col-md-5">
-                                        <img id="portrait" src="/images/ken-portrait.jpg"
-                                            className="img-fluid img-thumbnail mx-auto" alt="Kenneth I Petiote Portrait"></img>
-                                    </div>
-                                </div>
-                                <br></br>
+                                <a href="https://github.com/KPetiote">
+                                    <i className="fab fa-github-square fa-3x color-green padding-right"></i>
+                                </a>  
 
+                                <a href="https://www.linkedin.com/in/kenneth-petiote/">
+                                    <i className="fab fa-linkedin fa-3x color-green"></i>
+                                </a>
+                            </div>
+                            <div className="col-xs-7 col-md-5">
+                                <img id="portrait" src="/images/ken-portrait.jpg"
+                                    className="img-fluid img-thumbnail mx-auto" alt="Kenneth I Petiote Portrait"></img>
                             </div>
                         </div>
-                    </main>
-            </div>
+                        <br></br>
+                    </div>
+                </div>
+            </main>
         )
     }
 }
