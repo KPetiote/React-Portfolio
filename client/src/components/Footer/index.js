99ebf37b29117275e6f949ec9b382ccba12d671c
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-30 00:37:35 -0400

    Completed front page

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,12 +2,16 @@
 // ---------------------------------------------------------------------------
 
 import React from 'react';
-// import "./style.css";
+import "./style.css";
 
 function Footer() {
     return (
         <div>
-
+            <footer className="text-center fixed-bottom green">
+                <div className="card-header navbar-dark text-light background-green">
+                    <i className="far fa-copyright"></i> Kenneth I Petiote
+                </div>
+            </footer>
         </div>
     )
 }
